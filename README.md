6969bbff2961a3a85fd21fae2fc24317ec1ef4bb
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-04-30 16:51:19 +0530

    coded along with the lesson until API
    https://www.theodinproject.com/lessons/ruby-event-manager#assignment-day-of-the-week-targeting

--- README.md
+++
@@ -1,2 +0,0 @@
-# event_manager
-# event_manager
---
+++ lib/event_manager.rb
@@ -0,0 +1,22 @@
+require 'csv'
+
+def clean_zipcode(zipcode)
+    zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+puts "Event Manager Intialized!"
+
+contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+)
+
+contents.each do |row|
+    name = row[:first_name]
+
+    zipcode = clean_zipcode(row[:zipcode])
+
+    puts "#{name} #{zipcode}"
+end
+
